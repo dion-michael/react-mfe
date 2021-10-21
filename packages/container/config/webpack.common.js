de4998038e9efc4c304c267d4f34b55d9396cef6
dion-michael <dionagoes@gmail.com>
2021-10-21 09:59:46 +0800

    revert randomized classname

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -14,6 +14,13 @@ module.exports = {
                     },
                 },
             },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' }
+                ]
+            },
         ],
     },
     plugins: [
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,6 +3,7 @@ import MarketingApp from '../components/MarketingApp';
 import Header from '../components/Header';
 import { BrowserRouter } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+// import './index.css';
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co',
@@ -10,13 +11,13 @@ const generateClassName = createGenerateClassName({
 
 export default function App() {
     return (
-        <StylesProvider generateClassName={generateClassName}>
+        // <StylesProvider generateClassName={generateClassName}>
             <BrowserRouter>
                 <div>
                     <Header/>
                     <MarketingApp/>
                 </div>
             </BrowserRouter>
-        </StylesProvider>
+        // </StylesProvider>
     )
 }
